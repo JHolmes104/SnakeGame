e3943767f88a0c42bb38551cdea6f52f38b335e3
JACOB HOLMES <D3937345@live.tees.ac.uk>
2024-04-17 12:21:54 +0100

    Adjusted Win Screen Appearance

--- SnakeGame/WinScreen.cpp
+++ SnakeGame/WinScreen.cpp
@@ -1,5 +1,6 @@
 #include "WinScreen.h"
 #include <SFML/Graphics.hpp>
+#include "MainMenu.h"
 
 WinScreen::WinScreen(int winnerInput, int scoreInput)
 {
@@ -7,7 +8,7 @@ WinScreen::WinScreen(int winnerInput, int scoreInput)
 	score = scoreInput;
 
 	snakeFont.loadFromFile("SnakeChan-YdV8.ttf");
-	
+
 	if (winner == 0)
 	{
 		winnerColor = sf::Color::White;
@@ -26,35 +27,72 @@ WinScreen::WinScreen(int winnerInput, int scoreInput)
 
 void WinScreen::update(void)
 {
+	initialiseShapes();
+
 	sf::RenderWindow window(sf::VideoMode(800, 600), "Win Screen");
 	while (window.isOpen())
 	{
 		draw(window);
 
+		sf::Event event;
+		window.pollEvent(event);
+		switch (event.type)
+		{
+		case sf::Event::Closed:
+			window.close();
+			break;
+		default:
+			break;
+		}
 	}
 }
 
 void WinScreen::draw(sf::RenderWindow& window)
 {
-	sf::Text winnerText;
-	
+	window.clear(sf::Color::Black);
+	window.draw(winnerText);
+	window.draw(scoreText);
+	window.draw(mainMenuBackground);
+	window.draw(mainMenuText);
+	window.display();
+}
+
+void WinScreen::initialiseShapes(void)
+{
 	if (winner == 0)
 	{
 		winnerText.setString("Draw");
 		winnerText.setPosition(sf::Vector2f(340, 100));
 		winnerText.setCharacterSize(40);
+
+		scoreText.setString("Both players scored " + std::to_string(score) + " points!");
+		scoreText.setPosition(sf::Vector2f(75, 200));
+		scoreText.setCharacterSize(30);
 	}
 	else
 	{
 		winnerText.setString("Player " + std::to_string(winner) + " Wins!");
-		winnerText.setPosition(sf::Vector2f(235, 100));
+		winnerText.setPosition(sf::Vector2f(250, 100));
 		winnerText.setCharacterSize(30);
+
+		scoreText.setString("Player " + std::to_string(winner) + " scored " + std::to_string(score) + " points!");
+		scoreText.setPosition(sf::Vector2f(125, 200));
+		scoreText.setCharacterSize(30);
 	}
 
 	winnerText.setFont(snakeFont);
 	winnerText.setFillColor(winnerColor);
 
-	window.clear(sf::Color::Black);
-	window.draw(winnerText);
-	window.display();
+	scoreText.setFont(snakeFont);
+	scoreText.setFillColor(winnerColor);
+
+	mainMenuText.setString("Return to\nMain Menu");
+	mainMenuText.setPosition(sf::Vector2f(325, 420));
+	mainMenuText.setCharacterSize(20);
+	mainMenuText.setFont(snakeFont);
+	mainMenuText.setFillColor(sf::Color::Red);
+
+	mainMenuBackground.setPosition(sf::Vector2f(310, 410));
+	mainMenuBackground.setSize(sf::Vector2f(180, 70));
+	mainMenuBackground.setFillColor(sf::Color(128, 0, 0));
 }
--- SnakeGame/WinScreen.h
+++ SnakeGame/WinScreen.h
@@ -8,12 +8,19 @@ private:
 	int score;
 
 	void update(void);
+	void initialiseShapes(void);
 	void draw(sf::RenderWindow& window);
 
 	sf::Font snakeFont;
 	sf::Color winnerColor;
 
+	sf::Text winnerText;
+	sf::Text scoreText;
+	sf::Text mainMenuText;
+	sf::RectangleShape mainMenuBackground;
+
 public:
 	WinScreen(int winnerInput, int scoreInput);
 };
+
 
